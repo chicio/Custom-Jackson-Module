563521dfe29e3aa0580d895144e1c4ca92b4ff88
fduroni <fabrizio.duroni@gmail.com>
2022-03-14 17:45:59 +0100

    Added Carlo stuff to align object mapper to the spring one + fix modules names

--- src/main/kotlin/it/chicio/modulejackson/ProductConfiguration.kt
+++ src/main/kotlin/it/chicio/modulejackson/ProductConfiguration.kt
@@ -1,6 +1,7 @@
 package it.chicio.modulejackson
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -14,6 +15,8 @@ class ProductConfiguration {
     @Bean
     @Primary
     fun objectMapper(): ObjectMapper {
-        return ObjectMapper().findAndRegisterModules()
+        return ObjectMapper()
+            .findAndRegisterModules()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
     }
 }
